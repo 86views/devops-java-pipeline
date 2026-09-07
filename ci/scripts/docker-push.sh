cdd01a08d322d5c19813c3c1994250b6a91af95b
Oluleye Oluseun <oluseun.oluleye@gmail.com>
2026-09-07 16:07:00 +0100

    Fixing the pushing to the AWS ECR

--- ci/scripts/docker-push.sh
+++ ci/scripts/docker-push.sh
@@ -2,17 +2,17 @@
 set -euo pipefail
 
 IMAGE_TAG=${1:-"latest"}
-APP_NAME=${2:-"spring-petclinic"}
+LOCAL_IMAGE_NAME=${2:-"spring-petclinic"}
+ECR_REPO_NAME=${3:-"$LOCAL_IMAGE_NAME"}
 
-# AWS ECR Configuration
 AWS_ACCOUNT_ID=${AWS_ACCOUNT_ID:-"173331852212"}
 AWS_REGION=${AWS_REGION:-"us-east-1"}
 ECR_REGISTRY="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
 
-FULL_IMAGE="${ECR_REGISTRY}/${APP_NAME}:${IMAGE_TAG}"
+FULL_IMAGE="${ECR_REGISTRY}/${ECR_REPO_NAME}:${IMAGE_TAG}"
 
-echo "===> Tagging local image ${APP_NAME}:${IMAGE_TAG} to ${FULL_IMAGE}..."
-docker tag "${APP_NAME}:${IMAGE_TAG}" "${FULL_IMAGE}"
+echo "===> Tagging local image ${LOCAL_IMAGE_NAME}:${IMAGE_TAG} to ${FULL_IMAGE}..."
+docker tag "${LOCAL_IMAGE_NAME}:${IMAGE_TAG}" "${FULL_IMAGE}"
 
 echo "===> Pushing image to AWS ECR: ${FULL_IMAGE}..."
 docker push "${FULL_IMAGE}"
